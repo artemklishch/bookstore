ee83a498544659e8bf658b40e1e1331cafdd1bae
Artem Klishch <artemklishch@gmail.com>
2024-07-19 09:53:27 +0300

    fix: updated validation annotations

--- src/main/java/org/example/intro/dto/CreateBookDto.java
+++ src/main/java/org/example/intro/dto/CreateBookDto.java
@@ -1,26 +1,26 @@
 package org.example.intro.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateBookDto {
-    @NotNull
+    @NotBlank
     String title;
-
-    @NotNull
+    @NotBlank
     String author;
-
     @NotNull
     @Min(0)
     BigDecimal price;
-
+    @Size(min = 2, max = 255)
     String description;
-
-    @NotNull
+    @NotBlank
     String isbn;
-
+    @Size(min = 10)
     String coverImage;
 }
