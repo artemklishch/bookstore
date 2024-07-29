82f3383051cf1d9724536b2b0e8dd985aa1e4356
Artem Klishch <artemklishch@gmail.com>
2024-07-29 15:46:36 +0300

    fix: removed extra annotation

--- src/main/java/org/example/intro/service/UserService.java
+++ src/main/java/org/example/intro/service/UserService.java
@@ -3,9 +3,7 @@ package org.example.intro.service;
 import org.example.intro.dto.user.UserRegistrationRequestDto;
 import org.example.intro.dto.user.UserResponseDto;
 import org.example.intro.exceptions.RegistrationException;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
     UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException;
