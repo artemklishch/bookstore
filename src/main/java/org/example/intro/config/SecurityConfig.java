97448130cf15d100a77adac10731e3fdcd107e51
Artem Klishch <artemklishch@gmail.com>
2024-08-04 13:02:35 +0300

    feat: should add JWT authentication
    BREAKING CHANGES:
    
    *Added authentication token classes
    
    Task: https://mate.academy/learn/java-spring-boot/java-spring-boot-security-jwt/linear?learnItemsFilter=All&section=practice&videoId=5197&theoryId=3217&testTaskSlug=java_add_jwt_support

--- src/main/java/org/example/intro/config/SecurityConfig.java
+++ src/main/java/org/example/intro/config/SecurityConfig.java
@@ -2,16 +2,21 @@ package org.example.intro.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import lombok.RequiredArgsConstructor;
+import org.example.intro.sequrity.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -19,6 +24,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -43,7 +49,16 @@ public class SecurityConfig {
                                 .authenticated()
                 )
                 .httpBasic(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/org/example/intro/controller/AuthenticationController.java
+++ src/main/java/org/example/intro/controller/AuthenticationController.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.intro.dto.user.UserLoginRequestDto;
+import org.example.intro.dto.user.UserLoginResponseDto;
 import org.example.intro.dto.user.UserRegistrationRequestDto;
 import org.example.intro.dto.user.UserResponseDto;
 import org.example.intro.exceptions.RegistrationException;
+import org.example.intro.sequrity.AuthenticationService;
 import org.example.intro.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     @Operation(summary = "Register user", description = "Registers new user")
@@ -29,5 +33,13 @@ public class AuthenticationController {
             @RequestBody @Valid UserRegistrationRequestDto requestDto
     ) throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @Operation(summary = "Login user", description = "Login user")
+    @PostMapping("/login")
+    public UserLoginResponseDto login(
+            @RequestBody @Valid UserLoginRequestDto requestDto
+    ) {
+        return authenticationService.authenticate(requestDto);
     }
 }
