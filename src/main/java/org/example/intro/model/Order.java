50466ad4119f6e49fe8e8f6ab109e909c074822e
Artem Klishch <artemklishch@gmail.com>
2024-08-15 14:41:06 +0300

    feature: should add orders/order_items tables

--- src/main/java/org/example/intro/model/Order.java
+++ src/main/java/org/example/intro/model/Order.java
@@ -13,6 +13,9 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
